a2a8d4954b2a24e7dd9f34b7205d080b98abbddc
AChenny <anderson_chen97@hotmail.com>
2019-08-29 11:39:06 -0700

    Horizontal drag updated
    -updated the drag to work on pixels, and created the parameters on that such that the inside of each box goes from 0-600px, instead of the width being a percentage initially

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ $(document).ready(function() {
     //Prevent default to prevent the weird cursor thing when dragging a div.
     e.preventDefault();
 
-    dragging = true;
+    horzDragging = true;
     startPosX = e.pageX - $('#bar').offset().left;
 
   });
   $(document).mousemove(function(e){
 
-    if (dragging) {
+    if (horzDragging) {
       var posInGraphX = e.pageX - $('#bar').offset().left;
 
       offset = posInGraphX - startPosX;
@@ -27,16 +27,23 @@ $(document).ready(function() {
       var newLeftWidth = Math.round(10* ($('#leftBar').outerWidth()))/10 + offset;
       var newRightWidth = Math.round(10* ($('#rightBar').outerWidth()))/10 - offset;
 
+      if (newLeftWidth < 5) {
+        newLeftWidth = 5;
+        newRightWidth = 605;
+      }
+      else if (newLeftWidth > 605) {
+        newLeftWidth = 605;
+        newRightWidth = 5;
+      }
       $('#leftBar').css('width', newLeftWidth);
       $('#rightBar').css('width', newRightWidth);
 
       startPosX = posInGraphX;
-      console.log(offset);
     }
   });
 
   $(document).mouseup(function(e) {
-    dragging = false;
+    horDragging = false;
     // offSetX = startPosX - e.pageX;
   });
 });
