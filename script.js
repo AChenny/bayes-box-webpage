cd81862bc42b55c667e3638433dcb82af1a2a1d8
AChenny <anderson_chen97@hotmail.com>
2020-05-17 14:50:54 -0700

    Changed ID of switches for modes

--- script.js
+++ script.js
@@ -323,7 +323,7 @@ $(document).ready(function() {
   });
 
   $(function() {
-    $('#negative_result_mode_toggle_switch input').change(function() {
+    $('#negative_result_mode_toggle_switch').change(function() {
       if ($(this).prop('checked')) {
         toggleVisualEstimator(1);
       }
