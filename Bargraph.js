c7acb42117b4a6ec4317d6cee8c981314f1619ce
aca215 <anderson_chen@sfu.ca>
2021-04-30 22:51:29 -0700

    WIP: Negative results prototype with new bargraph class

---
+++ Bargraph.js
@@ -0,0 +1,283 @@
+import * as rulers from "./boxGraph/rulers.js";
+import * as interactivity from './boxGraph/interactivity.js';
+import { updateValues } from '../script.js';
+
+const CANVAS_BORDER_THICKNESS = 8;
+const CANVAS_BORDER_COLOR = '#000000';
+
+const LEFT_BAR_COLOR = '#90FF93';
+const RIGHT_BAR_COLOR = '#9090FF';
+const LEFT_BAR_COLOR_DESATURATED = '#1aff93';
+const RIGHT_BAR_COLOR_DESATURATED = '#c890ff';
+
+const LEFT_BAR_LINE_THICKNESS = 4;
+const RIGHT_BAR_LINE_THICKNESS = 4;
+const NEGATIVE_RESULTS_BAR_LINE_THICKNESS = 6;
+
+const LEFT_BAR_LINE_COLOR = '#000000'
+const RIGHT_BAR_LINE_COLOR = '#000000'
+const DESATURATED_BAR_LINE_COLOR = '#A9A9A9'
+const NEGATIVE_RESULTS_BAR_LINE_COLOR = '#000000'
+
+const MIDDLE_BAR_LINE_THICKNESS = 4;
+const MIDDLE_BAR_LINE_COLOR = '#B22222';
+
+const BOX_FONT_DEFAULT_STYLE = 'Verdana';
+const BOX_FONT_DEFAULT_WEIGHT = 'bolder';
+
+const LEFT_BOX_FONT_MAX_SIZE = 22; // In pt font
+const RIGHT_BOX_FONT_MAX_SIZE = 18; // In pt font
+const BOX_FONT_DEFAULT_COLOR = '#000000';
+const BOX_FONT_BUFFER = 70; // In pixels
+
+export default class BarGraph {
+    constructor(canvas, ruler_left_container, ruler_right_container, ruler_mid_container, estimator_mode = false, negative_results_mode = false) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.ruler_left_container = ruler_left_container;
+        this.ruler_right_container = ruler_right_container;
+        this.ruler_mid_container = ruler_mid_container;
+        this.estimator_mode = estimator_mode;
+        this.negative_results_mode = negative_results_mode;
+        
+        this.canvas_width = canvas.width;
+        this.canvas_height = canvas.height;
+
+        this.left_bar_drag = false;
+        this.right_bar_drag = false;
+        this.middle_line_drag = false;
+        
+        this.leftY = 0;
+        this.rightY = 0;
+        this.middleX = 0;
+    }
+
+    update() {
+        clear();
+        drawBoxGraph(pEH, pENotH, pH);
+        rulers.updateRulers(pEH, pENotH, pH);
+        if (!this.estimator_mode && !this.negative_results_mode) {
+            this.drawBoxText(pEH, pENotH, pH);
+        }
+        this.drawBorder();
+        
+        // requestAnimationFrame(update);
+    }
+
+    // Description: Clears the boxGraph of drawings
+    clear() {
+        this.ctx.clearRect(0, 0, this.canvas_width, this.canvas_height);
+    }
+    
+    // Description: Draws a border around the canvas
+    drawBorder() {
+        this.ctx.lineWidth = CANVAS_BORDER_THICKNESS;
+        this.ctx.strokeStyle = CANVAS_BORDER_COLOR;
+        this.ctx.strokeRect(0, 0, this.canvas_width, this.canvas_height);
+    }
+
+    // Description: Draws the box graph
+    // Input: P(E|H) floating point value, P(E|Not-H) floating point value, P(H) floating point value
+    // Example: drawBoxGraph(0.32, 0.65, 0.64) draws a graph with left bar 32% height, right bar 65% height, and middle divider 64% to the right
+    drawBoxGraph(pEH=0.5, pENotH=0.5, pH=0.5) {
+        this.clear();
+        // Note: When using the percentages and using it in the canvas, it must be inverted
+        // Draw the left bar
+        let leftBarHeight = (1-pEH) * this.canvas_height;
+        let leftBarWidth = pH * this.canvas_width;
+        
+        if (this.negative_results_mode) {
+            this.ctx.fillStyle = LEFT_BAR_COLOR_DESATURATED;
+        }
+        else {
+            this.ctx.fillStyle = LEFT_BAR_COLOR;
+        }
+        this.ctx.fillRect(0, leftBarHeight, leftBarWidth, this.canvas_height);
+        
+        // Draw the left bar height line
+        if (this.negative_results_mode) {
+            this.ctx.strokeStyle = DESATURATED_BAR_LINE_COLOR;
+        }
+        else {
+            this.ctx.strokeStyle = LEFT_BAR_LINE_COLOR;
+        }
+        this.ctx.lineWidth = LEFT_BAR_LINE_THICKNESS;
+        this.ctx.beginPath();
+        this.ctx.moveTo(0, leftBarHeight);
+        this.ctx.lineTo(leftBarWidth, leftBarHeight);
+        this.ctx.stroke();
+        this.leftY = leftBarHeight;
+        
+        // Draw the right bar
+        let rightBarHeight = (1-pENotH) * this.canvas_height;
+        let rightBarWidth = pH * this.canvas_width;
+        
+        if (this.negative_results_mode) {
+            this.ctx.fillStyle = RIGHT_BAR_COLOR_DESATURATED;
+        }
+        else {
+            this.ctx.fillStyle = RIGHT_BAR_COLOR;
+        }
+        this.ctx.fillRect(rightBarWidth, rightBarHeight, this.canvas_width, this.canvas_height);
+        
+        // Draw the right bar height line
+        if (this.negative_results_mode) {
+            this.ctx.strokeStyle = DESATURATED_BAR_LINE_COLOR;
+        }
+        else {
+            this.ctx.strokeStyle = RIGHT_BAR_LINE_COLOR;
+        }
+        this.ctx.lineWidth = RIGHT_BAR_LINE_THICKNESS;
+        this.ctx.beginPath();
+        this.ctx.moveTo(rightBarWidth, rightBarHeight);
+        this.ctx.lineTo(this.canvas_width, rightBarHeight);
+        this.ctx.stroke();
+        this.rightY = rightBarHeight;
+        
+        // Draw the middle line
+        let middleLineX = pH * this.canvas_width;
+        this.ctx.strokeStyle = MIDDLE_BAR_LINE_COLOR;
+        this.ctx.lineWidth = MIDDLE_BAR_LINE_THICKNESS;
+        this.ctx.beginPath();
+        this.ctx.moveTo(middleLineX, 0);
+        this.ctx.lineTo(middleLineX, this.canvas_height);
+        this.ctx.stroke();
+        this.middleX = middleLineX;
+
+        // Negative results border
+        if (this.negative_results_mode) {
+            this.ctx.strokeStyle = NEGATIVE_RESULTS_BAR_LINE_COLOR;
+            this.ctx.lineWidth = NEGATIVE_RESULTS_BAR_LINE_THICKNESS;
+            this.ctx.beginPath();
+            this.ctx.rect(0, 0, this.middleX-MIDDLE_BAR_LINE_THICKNESS, leftBarHeight);
+            this.ctx.stroke();
+
+        }
+        this.drawBorder();
+        if (!this.negative_results_mode) {
+            this.drawBoxText(pEH, pENotH, pH);
+        }
+        rulers.updateRulers(pEH, pENotH, pH);
+        
+        updateValues(pEH, pENotH, pH);
+    }
+
+    // Description: Draws the graph percentages inside the bar graphs
+    // Input: Input: P(E|H) floating point value, P(E|Not-H) floating point value, P(H) floating point value
+    drawBoxText(pEH=0.5, pENotH=0.5, pH=0.5) {
+        let pHE = ((pEH * pH)/((pEH * pH) + (pENotH * (1 - pH)))).toFixed(3);
+        
+        let leftBarFontSize = LEFT_BOX_FONT_MAX_SIZE;
+        let rightBarFontSize =  RIGHT_BOX_FONT_MAX_SIZE;
+        // Check left bar size
+        if (pEH < 0.15 || pH < 0.15) {
+            // Take the smaller size font and set it as that
+            let percentageFont = Math.min((pEH * 6.66), (pH * 6.66));
+            leftBarFontSize = (percentageFont * LEFT_BOX_FONT_MAX_SIZE).toFixed(0);
+        } 
+        if (pENotH < 0.15 || pH > 0.85) {
+            // Take the smaller size font and set it as that
+            let percentageFont = Math.min((pENotH * 6.66), ((1-pH) * 6.66));
+            rightBarFontSize = (percentageFont * RIGHT_BOX_FONT_MAX_SIZE).toFixed(0);
+        }
+        
+        // Add the text to the middle of the boxes
+        let leftBarTextCoordsX = (((this.middleX-BOX_FONT_BUFFER) / 2)).toFixed(0);
+        let leftBarTextCoordsY = ((this.canvas_height + this.leftY) / 2).toFixed(0);
+        this.ctx.fillStyle = BOX_FONT_DEFAULT_COLOR;
+        this.ctx.font = BOX_FONT_DEFAULT_WEIGHT + ' ' + leftBarFontSize + 'pt ' +  BOX_FONT_DEFAULT_STYLE;
+        this.ctx.fillText(pHE, leftBarTextCoordsX, leftBarTextCoordsY);
+
+        let rightBarTextCoordsX = (((this.middleX + this.canvas_width) - BOX_FONT_BUFFER)/2).toFixed(0);
+        let rightBarTextCoordsY = ((this.canvas_height + this.rightY) / 2).toFixed(0);
+        this.ctx.fillStyle = BOX_FONT_DEFAULT_COLOR;
+        this.ctx.font = rightBarFontSize + 'pt ' + BOX_FONT_DEFAULT_STYLE + ' '; 
+        this.ctx.fillText((1-pHE).toFixed(3), rightBarTextCoordsX, rightBarTextCoordsY);
+    }
+
+    // Description: Resets the drag booleans
+    resetDragState() {
+        this.left_bar_drag = false;
+        this.right_bar_drag = false;
+        this.middle_line_drag = false;
+    }
+
+    // Description: Changes all instances of E (evidence) to the negative
+    // Input: state ? true (turn to E): false (turn to NOT-E)
+    change_e_to_not_e (state){
+        let e_text;
+        e_text = state ? "&not;E" :"E";
+        // Change text in evidence strength text
+        $('#buttons_evidence_strength_formula_numerator').html(`<b><i>P</i></b>(${e_text}<b>|</b>H)`);
+        $('#buttons_evidence_strength_formula_denominator').html(`<b><i>P</i></b>(${e_text}<b>|</b>&not;H)`);
+
+        // Change text in Formula
+        $('#formula_phe_text').html(`<p><b><i>P</i></b>(H<b>|</b>${e_text})</p>`);
+        $('#formula_peh_top_text').html(`<b><i>P</i></b>(${e_text}<b>|</b>H)`);
+        $('#formula_peh_bottom_text').html(`<b><i>P</i></b>(${e_text}<b>|</b>H)`);
+        $('#formula_p_not_h_bottom_text').html(`<b><i>P</i></b>(${e_text}<b>|</b>&not;H)`);
+    }
+
+    
+    // Description: Flips the rulers to positive results mode or negative results mode
+    // Input: Boolean
+    // Output: None
+    setNegativeResultsRuler(flag) {
+        // Negative results mode is on, set the rulers to P(H|E)
+        this.change_e_to_not_e(flag);
+        if (flag) {
+            // Hide the positive rulers
+            $('#verticalRulerLeftBottom').css('visibility', 'hidden');
+            $('#verticalRulerRightBottom').css('visibility', 'hidden');
+
+            // Show the negative rulers
+            $('#verticalRulerLeftTop').css('visibility', 'visible');
+            $('#verticalRulerRightTop').css('visibility', 'visible');
+
+        }
+        else {
+            // Hide the negative rulers
+            $('#verticalRulerLeftTop').css('visibility', 'hidden');
+            $('#verticalRulerRightTop').css('visibility', 'hidden');
+            
+            // Show the positive rulers
+            $('#verticalRulerLeftBottom').css('visibility', 'visible');
+            $('#verticalRulerRightBottom').css('visibility', 'visible');
+        }
+    }
+
+    setNegativeResultsModeBoxGraph(flag) {
+        this.negative_results_mode = flag;
+    }
+    
+
+    handleCanvasMove(event, pEH, pENotH, pH) {
+        // Handle changing the cursor
+        interactivity.handleMouseCursor(event, this.leftY, this.rightY, this.middleX);
+        if (this.left_bar_drag) {
+            pEH = interactivity.handleVerticalBarDrag(event);
+            this.drawBoxGraph(pEH, pENotH, pH);
+        }
+        if (this.right_bar_drag) {
+            pENotH = interactivity.handleVerticalBarDrag(event);
+            this.drawBoxGraph(pEH, pENotH, pH);
+        }
+        if (this.middle_line_drag) {
+            pH = interactivity.handleHorizontalLineDrag(event);
+            this.drawBoxGraph(pEH, pENotH, pH);
+        }
+    }
+
+    handleCanvasMouseDown(event) {
+        // Update the interactivity booleans
+        if (interactivity.isMouseOnMiddleLine(event, this.middleX)) {
+            this.middle_line_drag = true;
+        }
+        else if (interactivity.isMouseOnLeftBarLine(event, this.leftY, this.middleX)) {
+            this.left_bar_drag = true;
+        }
+        else if (interactivity.isMouseOnRightBarLine(event, this.rightY, this.middleX)) {
+            this.right_bar_drag = true;
+        }
+    }
+}
--- script.js
+++ script.js
@@ -1,202 +1,254 @@
 // Import constants 
 import * as constants from './constants.js';
-import { updateBoxGraphValues, setEstimatorModeBoxGraph, setNegativeResultsModeBoxGraph, setNegativeResultsRuler} from './boxGraph/main.js';
+// import { updateBoxGraphValues, setEstimatorModeBoxGraph, setNegativeResultsModeBoxGraph, setNegativeResultsRuler} from './boxGraph/main.js';
+import BarGraph from "/BarGraph.js";
 
 // Global Variables
 var pEH = 0.500;
-var pENotH = 0.500;
-var pH = 0.500;
+var pENotH = 0.200;
+var pH = 0.400;
+
+var negative_results_mode = false;
+
+// Class objects
+const canvas = document.getElementById('boxGraph');
+var barGraph = new BarGraph(canvas, null, null, null);
 
 // Description: Exportable value updater for this scope
 // Input: Probability values in float <1;
 // Example: updateBoxValues(0.500, 0.522, 1.000);
 export function updateValues(_pEH, _pENotH, _pH) {
-  pEH = parseFloat(_pEH);
-  pENotH = parseFloat(_pENotH);
-  pH = parseFloat(_pH);
-  
-  updateFormula(pEH, pENotH, pH);
-  _updateLights(pEH, pENotH, pH);
+    pEH = parseFloat(_pEH);
+    pENotH = parseFloat(_pENotH);
+    pH = parseFloat(_pH);
+
+    updateFormula(pEH, pENotH, pH);
+    updateLights(pEH, pENotH, pH);
 }
 
 function updateFormula(pEH, pENotH, pH) {
-  // If the user is using any of the input boxes, don't update it.
-  if (!$("#box1").is(":focus")) {
-    var box1 = document.getElementById('box1');
-    box1.value = pEH.toFixed(3);
-  };
-  document.getElementById('equation_values_denominator_peh').innerHTML = pEH.toFixed(3);
-  
-  // Box 2 
-  if (!$("#box2").is(":focus")) {
-    var box2 = document.getElementById('box2');
-    box2.value = pH.toFixed(3);
-  };
-  document.getElementById('equation_values_denominator_ph').innerHTML = pH.toFixed(3);
+    let form_pEH;
+    let form_pENotH;
 
-  // Box 3
-  if (!$("#box3").is(":focus")) {
-    var box3 = document.getElementById('box3');
-    box3.value = pENotH.toFixed(3);
-  };
+    if (negative_results_mode) {
+        form_pEH = 1 - pEH;
+        form_pENotH = 1 - pENotH;
+    }
+    else {
+        form_pEH = pEH;
+        form_pENotH = pENotH;
+    }
+    
+    // If the user is using any of the input boxes, don't update it.
+    if (!$("#box1").is(":focus")) {
+        var box1 = document.getElementById('box1');
+        box1.value = form_pEH.toFixed(3);
+    };
+    document.getElementById('equation_values_denominator_peh').innerHTML = form_pEH.toFixed(3);
 
-  //Update formula text 1 P(NOT-H)
-  var formVar1 = document.getElementById('formVar1');
-  formVar1.innerHTML = (1 - pH).toFixed(3);
-  //update var2
-  var formVar2 = document.getElementById('formVar2');
-  var pHE = (pEH * pH)/((pEH * pH) + (pENotH * (1 - pH)));
-  formVar2.innerHTML = pHE.toFixed(3);
+    // Box 2 
+    if (!$("#box2").is(":focus")) {
+        var box2 = document.getElementById('box2');
+        box2.value = pH.toFixed(3);
+    };
+    document.getElementById('equation_values_denominator_ph').innerHTML = pH.toFixed(3);
 
-  // Update the box graph values
-  updateBoxGraphValues(pEH, pENotH, pH);
+    // Box 3
+    if (!$("#box3").is(":focus")) {
+        var box3 = document.getElementById('box3');
+        box3.value = form_pENotH.toFixed(3);
+    };
+
+    //Update formula text 1 P(NOT-H)
+    var formVar1 = document.getElementById('formVar1');
+    formVar1.innerHTML = (1 - pH).toFixed(3);
+    //update var2
+    var formVar2 = document.getElementById('formVar2');
+    var pHE = (form_pEH * pH) / ((form_pEH * pH) + (form_pENotH * (1 - pH)));
+    formVar2.innerHTML = pHE.toFixed(3);
+
+    // Update the box graph values
+    // barGraph.updateBoxGraphValues(pEH, pENotH, pH);
 }
 
 function toggleVisualEstimator(toggleOn) {
-  if (toggleOn) {
-    $('#formVar1').hide();
-    $('#formVar2').hide();
-    $('#believe_confirmation_evidence_strength_value_span').hide();
-    $('#box1').css('color', '#FFFFFF');
-    $('#box2').css('color', '#FFFFFF');
-    $('#box3').css('color', '#FFFFFF');
-    $('#equation_values_denominator_peh').hide();
-    $('#equation_values_denominator_ph').hide();
-    
-  }
-  else {
-    $('#formVar1').show();
-    $('#formVar2').show();
-    $('#believe_confirmation_evidence_strength_value_span').show();
-    $('#box1').css('color', '#000000');
-    $('#box2').css('color', '#000000');
-    $('#box3').css('color', '#000000');
-    $('#equation_values_denominator_peh').show();
-    $('#equation_values_denominator_ph').show();
-  }
+    if (toggleOn) {
+        $('#formVar1').hide();
+        $('#formVar2').hide();
+        $('#believe_confirmation_evidence_strength_value_span').hide();
+        $('#box1').css('color', '#FFFFFF');
+        $('#box2').css('color', '#FFFFFF');
+        $('#box3').css('color', '#FFFFFF');
+        $('#equation_values_denominator_peh').hide();
+        $('#equation_values_denominator_ph').hide();
+
+    }
+    else {
+        $('#formVar1').show();
+        $('#formVar2').show();
+        $('#believe_confirmation_evidence_strength_value_span').show();
+        $('#box1').css('color', '#000000');
+        $('#box2').css('color', '#000000');
+        $('#box3').css('color', '#000000');
+        $('#equation_values_denominator_peh').show();
+        $('#equation_values_denominator_ph').show();
+    }
 }
 
 // state: true for on, false for off
 function setNegativeResultsMode(state) {
-  setNegativeResultsModeBoxGraph(state);
-  setNegativeResultsRuler(state);
-  // Ensure button switch is also changed to correct position
-  let random_button = document.getElementById('buttons_negative_results_switch');
-  random_button.checked = state;
+    barGraph.setNegativeResultsModeBoxGraph(state);
+    barGraph.setNegativeResultsRuler(state);
+    barGraph.drawBoxGraph(pEH, pENotH, pH);
+    negative_results_mode = state;
+    
+    // Ensure button switch is also changed to correct position
+    let random_button = document.getElementById('buttons_negative_results_switch');
+    random_button.checked = state;
+    
+    // Change the values shown in the formula
+    updateFormula(pEH, pENotH, pH);
+    updateLights(pEH, pENotH, pH);
 }
 
+
 function randomizeButton() {
-  pEH = parseFloat(Math.random(1000).toFixed(3));
-  pH = parseFloat(Math.random(1000).toFixed(3));
-  pENotH = parseFloat(Math.random(1000).toFixed(3));
-  updateFormula(pEH, pENotH, pH);
-  _updateLights(pEH, pENotH, pH)
-  updateBoxGraphValues(pEH, pENotH, pH);
-  // randomize lights negative results mode as well
-  setNegativeResultsMode(Math.random() < 0.5);
-  
+    pEH = parseFloat(Math.random(1000).toFixed(3));
+    pH = parseFloat(Math.random(1000).toFixed(3));
+    pENotH = parseFloat(Math.random(1000).toFixed(3));
+    updateFormula(pEH, pENotH, pH);
+    updateLights(pEH, pENotH, pH)
+    barGraph.drawBoxGraph(pEH, pENotH, pH);
+    // randomize lights negative results mode as well
+    setNegativeResultsMode(Math.random() < 0.5);
+
 }
 
 // Description: Updates the Evidence strength, prior probability, and updated probability saturation
 // Input: Probability values (pEH, pENotH, pH)
 // Example: _updateLights(0.500, 0.522, 0.662)
-function _updateLights(pEH, pENotH, pH) {
-  let pHE = ((pEH * pH)/((pEH * pH) + (pENotH * (1 - pH)))).toFixed(3);
+function updateLights(pEH, pENotH, pH) {
+    let pHE = ((pEH * pH) / ((pEH * pH) + (pENotH * (1 - pH)))).toFixed(3);
 
-  let rgbToHex = function (rgb) { 
-    let hex = Number(rgb).toString(16);
-    if (hex.length < 2) {
-      hex = "0" + hex;
+    let rgbToHex = function (rgb) {
+        let hex = Number(rgb).toString(16);
+        if (hex.length < 2) {
+            hex = "0" + hex;
+        }
+        return hex;
+    };
+
+    // Description: Gets the number and return the next highest number in the logspace array using a binary search
+    // Input: Some positive number x
+    // Output: Position in the logspace constant
+    let getPosInLogspace = function (x) {
+        let start = 0, end = constants.logspace.length - 1;
+        // Exceptions is if x is greater than max or min, set the saturation to that
+        if (x < constants.MIN_EVIDENCE_STRENGTH_SATURATION) {
+            return 0;
+        }
+        if (x > constants.MAX_EVIDENCE_STRENGTH_SATURATION) {
+            return 254;
+        }
+        while (start <= end) {
+            // Iterate until You find a two numbers that this number can fit in
+            let i = Math.floor((start + end) / 2);
+            if (constants.logspace[i - 1] < x && x <= constants.logspace[i]) return i;
+
+            else if (constants.logspace[i] < x) {
+                start = i + 1
+            }
+            else {
+                end = i - 1;
+            }
+        }
     }
-    return hex;
-  };
 
-  // Description: Gets the number and return the next highest number in the logspace array using a binary search
-  // Input: Some positive number x
-  // Output: Position in the logspace constant
-  let getPosInLogspace = function(x) {
-    let start=0, end=constants.logspace.length-1;
-    // Exceptions is if x is greater than max or min, set the saturation to that
-    if (x < constants.MIN_EVIDENCE_STRENGTH_SATURATION) {
-      return 0;
+    let probableSaturation = rgbToHex(parseInt((pH * 255)));
+    $('#believe_confirmation_prior_probability_light').css('background-color', '#' + probableSaturation + probableSaturation + probableSaturation);
+    $('#believe_confirmation_prior_probability_light_text').css('color', '#' + rgbToHex(parseInt(((1 - pH) * 255))) + rgbToHex(parseInt(((1 - pH) * 255))) + rgbToHex(parseInt(((1 - pH) * 255))));
+
+    // Change the brightness of the evidence strength light on probability change
+    let evidenceStrengthCalculation;
+    if (negative_results_mode) {
+        evidenceStrengthCalculation = (1-pEH) / pENotH;
     }
-    if (x > constants.MAX_EVIDENCE_STRENGTH_SATURATION) {
-      return 254;
+    else {
+        evidenceStrengthCalculation = pEH / pENotH;
     }
-    while (start<=end) {
-      // Iterate until You find a two numbers that this number can fit in
-      let i = Math.floor((start + end)/2);
-      if (constants.logspace[i-1] < x  && x <= constants.logspace[i] ) return i;
 
-      else if (constants.logspace[i] < x) {
-        start = i + 1
-      } 
-      else {
-        end = i - 1;
-      }
+    if (isFinite(evidenceStrengthCalculation)) {
+        $('#believe_confirmation_evidence_strength_value_span').text(evidenceStrengthCalculation.toFixed(2));
     }
-  }
+    else {
+        $('#believe_confirmation_evidence_strength_value_span').text('Maximum');
+    }
 
-  let probableSaturation = rgbToHex(parseInt((pH * 255)));
-  $('#believe_confirmation_prior_probability_light').css('background-color', '#' +  probableSaturation + probableSaturation + probableSaturation);
-  $('#believe_confirmation_prior_probability_light_text').css('color', '#' +  rgbToHex(parseInt(((1-pH) * 255))) + rgbToHex(parseInt(((1-pH) * 255))) + rgbToHex(parseInt(((1-pH) * 255))));
-  
-  // Change the brightness of the evidence strength light on probability change
-  let evidenceStrengthCalculation = pEH/pENotH;
+    let confirmationRHex = rgbToHex(parseInt(144));
+    let confirmationGHex = rgbToHex(parseInt(144 + ((evidenceStrengthCalculation / 2) * 111)));
+    let confirmationBHex = rgbToHex(parseInt(144 + ((1 - (evidenceStrengthCalculation / 2)) * 111)));
+    $('#believe_confirmation_evidence_strength_light').css('background-color', '#' + confirmationRHex + confirmationGHex + confirmationBHex);
 
-  if (isFinite(evidenceStrengthCalculation)) {
-    $('#believe_confirmation_evidence_strength_value_span').text(evidenceStrengthCalculation.toFixed(2));
-  }
-  else {
-    $('#believe_confirmation_evidence_strength_value_span').text('Maximum');
-  }
-
-  let confirmationRHex = rgbToHex(parseInt(144));
-  let confirmationGHex = rgbToHex(parseInt(144 + ((evidenceStrengthCalculation/2) * 111)));
-  let confirmationBHex = rgbToHex(parseInt(144 + ((1-(evidenceStrengthCalculation/2)) * 111)));
-  $('#believe_confirmation_evidence_strength_light').css('background-color', '#' + confirmationRHex + confirmationGHex  + confirmationBHex );
-
-  // Change the brightness of the updated probability light and the text to inverse
-  let updatedProbabilitySaturation = rgbToHex(parseInt(pHE*255));
-  $('#believe_confirmation_updated_probability_light').css('background-color', '#' + updatedProbabilitySaturation + updatedProbabilitySaturation + updatedProbabilitySaturation);
-  $('#believe_confirmation_updated_probability_light_text').css('color', '#' + rgbToHex(parseInt((1-pHE)*255)) + rgbToHex(parseInt((1-pHE)*255)) + rgbToHex(parseInt((1-pHE)*255)));
+    // Change the brightness of the updated probability light and the text to inverse
+    let updatedProbabilitySaturation = rgbToHex(parseInt(pHE * 255));
+    $('#believe_confirmation_updated_probability_light').css('background-color', '#' + updatedProbabilitySaturation + updatedProbabilitySaturation + updatedProbabilitySaturation);
+    $('#believe_confirmation_updated_probability_light_text').css('color', '#' + rgbToHex(parseInt((1 - pHE) * 255)) + rgbToHex(parseInt((1 - pHE) * 255)) + rgbToHex(parseInt((1 - pHE) * 255)));
 }
 
-$(document).ready(function() {
-  _updateLights(pEH, pENotH, pH)
+// Description: Initializes bargraph with starting values
+function initBarGraph() {
+    barGraph.drawBoxGraph(pEH, pENotH, pH);
+    barGraph.drawBorder();
+}
 
-  $('#randomize_button').click(randomizeButton);
-  
-  //On return key press
-  $(document).on('keypress',function(e) {
-    if(e.which == 13) {
-        pEH = parseFloat($("input[name=box1]").val());
-        pH = parseFloat($("input[name=box2]").val());
-        pENotH = parseFloat($("input[name=box3]").val());
-        
-        // Unfocus the box to allow for update
-        $('#box1').blur()
-        $('#box2').blur()
-        $('#box3').blur()
-        updateFormula(pEH, pENotH, pH)
-    }
-  });
+$(document).ready(function () {
+    updateLights(pEH, pENotH, pH)
+    initBarGraph();
 
-  $(function() {
-    $('#buttons_estimator_switch').change(function() {
-      if ($(this).prop('checked')) {
-        toggleVisualEstimator(1);
-        setEstimatorModeBoxGraph(true);
-      }
-      else {
-        toggleVisualEstimator(0);
-        setEstimatorModeBoxGraph(false);
-      }
+    $('#randomize_button').click(randomizeButton);
+
+    //On return key press
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            pEH = parseFloat($("input[name=box1]").val());
+            pH = parseFloat($("input[name=box2]").val());
+            pENotH = parseFloat($("input[name=box3]").val());
+
+            // Unfocus the box to allow for update
+            $('#box1').blur()
+            $('#box2').blur()
+            $('#box3').blur()
+            updateFormula(pEH, pENotH, pH)
+        }
+    });
+
+    $(function () {
+        $('#buttons_estimator_switch').change(function () {
+            if ($(this).prop('checked')) {
+                toggleVisualEstimator(1);
+                setEstimatorModeBoxGraph(true);
+            }
+            else {
+                toggleVisualEstimator(0);
+                setEstimatorModeBoxGraph(false);
+            }
+        })
     })
-  })
 });
-$(function() {
-  $('#buttons_negative_results_switch').change(function() {
-    setNegativeResultsMode($(this).prop('checked'));
-  });
+$(function () {
+    $('#buttons_negative_results_switch').change(function () {
+        setNegativeResultsMode($(this).prop('checked'));
+    });
 })
+
+canvas.addEventListener('mousemove', function() {
+    barGraph.handleCanvasMove(event, pEH, pENotH, pH);
+});
+
+canvas.addEventListener('mousedown', function() {
+    barGraph.handleCanvasMouseDown(event);
+});
+
+document.addEventListener('mouseup', function() {
+    barGraph.resetDragState();
+});
